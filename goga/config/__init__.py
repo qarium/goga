9ebb4e35a3e1e8b0b807c3375960357b493e00b2
trifonovmixail <trifonov.net@gmail.com>
2026-07-27 21:19:54 +0000

    feat: re-export DepConfig from goga/config facade

--- goga/config/__init__.py
+++ goga/config/__init__.py
@@ -3,6 +3,7 @@ from .home.loader import load_home_config
 from .project.config import (
     BuildConfig,
     CodemanifestConfig,
+    DepConfig,
     PipelineConfig,
     ProjectConfig,
     TaskExecutorConfig,
@@ -12,6 +13,7 @@ from .project.loader import load_project_config
 __all__ = [
     "BuildConfig",
     "CodemanifestConfig",
+    "DepConfig",
     "DockerArgsConfig",
     "HomeConfig",
     "PipelineConfig",
